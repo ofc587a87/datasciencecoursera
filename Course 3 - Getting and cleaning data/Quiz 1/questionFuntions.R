7e8c45579f3304794ca135e8c10e3d64e3379e28
ofc587a87 <ofc587a87@gmail.com>
2015-05-09 22:42:34 +0200

    Question 3

--- Course 3 - Getting and cleaning data/Quiz 1/questionFuntions.R
+++ Course 3 - Getting and cleaning data/Quiz 1/questionFuntions.R
@@ -1,13 +1,20 @@
 
-readData <- function() {
-    # reads file
-    read.csv(file = "getdata-data-ss06hid.csv")
-}
+library("xlsx")
 
 question1 <- function() {
-    
-    data <- readData()
+    # reads file
+    data <- read.csv(file = "getdata-data-ss06hid.csv")
     
     # How many properties are worth $1,000,000 or more?
     message(paste("Num properties are worth $1,000,000 or more:", nrow(data[!is.na(data$VAL)& data$VAL == 24 ,])))
+}
+
+question3 <- function() {
+    # reads file, rows 18-23 and columns 7-15
+    data <- read.xlsx("getdata-data-DATA.gov_NGAP.xlsx", sheetIndex = 1, header=TRUE,
+                      startRow = 18, endRow=23);
+    
+    dat <- data[, 7:15];
+    
+    sum(dat$Zip*dat$Ext,na.rm=T);
 }
