ce5aad93e29f811dc13e1bcc89a94e609003f61e
ofc587a87 <ofc587a87@gmail.com>
2015-08-18 22:12:17 +0200

    Plot 1 completted

--- Course 4 - Exploratory Data Analysis/CourseProject2/Plot1.R
+++ Course 4 - Exploratory Data Analysis/CourseProject2/Plot1.R
@@ -18,7 +18,29 @@ createPlot1 <- function(){
         }
     }
     
-    plot(totalsPerYear, type="o", xlim=c(1998.5, 2008.5), ylim=c(3200000, 7500000));
+    # automaticis limits to fits text
+    xLimitsBias=(max(totalsPerYear[,1]) - min(totalsPerYear[,1])) / 20;
+    xLimits <- c(min(totalsPerYear[,1]) - xLimitsBias, max(totalsPerYear[,1]) + xLimitsBias)
+
+    yLimitsBias=(max(totalsPerYear[,2]) - min(totalsPerYear[,2])) / 20;
+    yLimits <- c(min(totalsPerYear[,2]) - yLimitsBias, max(totalsPerYear[,2]) + yLimitsBias)
+    
+    # Creates device
+    png(filename = "PLot1.png", width = 600, height = 480);
+    
+    # plot the data without lower axis
+    plot(totalsPerYear, type="o", xlim=xLimits, ylim=yLimits, xaxt="n");
+    
+    # add lower axis with all ytears detailed
+    axis(side=1, round(xLimits[1]):round(xLimits[2]));
+    
+    # add text data to each point
     text(totalsPerYear, as.character(round(totalsPerYear[,2], 2)), cex=0.8, pos=3, offset = 0.3, col = "red");
+    
+    # decorate plot
     title(main="Total emissions from PM2.5 in the United States");
+    
+    dev.off();
+    
+    message("Image plot1.png generated!");
 }
