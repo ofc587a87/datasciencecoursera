901324106984cf6707911bc2cfa043e0e68fa0a4
ofc587a87 <ofc587a87@gmail.com>
2015-08-19 18:46:11 +0200

    Exercise 2 completed

--- Course 4 - Exploratory Data Analysis/CourseProject2/Plot2.R
+++ Course 4 - Exploratory Data Analysis/CourseProject2/Plot2.R
@@ -1,18 +1,23 @@
-# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
-# Using the base plotting system, make a plot showing the total PM2.5
-# emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland
+# (fips == "24510") from 1999 to 2008? Use the base plotting system to make a
+# plot answering this question.
 
 source("utils.R");
 
-createPlot1 <- function(){
+createPlot2 <- function(){
     
     data <- NEI[[1]];
+    
+    #subset data for Baltimore
+    baltimoreData <- subset(data, fips == "24510");
+    
+    ###### SAME CODE AS createPlot1, but using baltimoreData #######
     
     # creates a new dataset with the totals per year
     totalsPerYear <- matrix(dimnames = list(NULL, c("Years", "Emissions")), ncol = 2, nrow = 0);
     
-    for(y in min(data$year):max(data$year)) {
-        s <- sum(data$Emissions[data$year == y]);
+    for(y in min(baltimoreData$year):max(baltimoreData$year)) {
+        s <- sum(baltimoreData$Emissions[baltimoreData$year == y]);
         if(s > 0 ) {
             totalsPerYear <- rbind(totalsPerYear, c(y, s));
         }
@@ -21,12 +26,12 @@ createPlot1 <- function(){
     # automaticis limits to fits text
     xLimitsBias=(max(totalsPerYear[,1]) - min(totalsPerYear[,1])) / 20;
     xLimits <- c(min(totalsPerYear[,1]) - xLimitsBias, max(totalsPerYear[,1]) + xLimitsBias)
-
+    
     yLimitsBias=(max(totalsPerYear[,2]) - min(totalsPerYear[,2])) / 20;
     yLimits <- c(min(totalsPerYear[,2]) - yLimitsBias, max(totalsPerYear[,2]) + yLimitsBias)
     
     # Creates device
-    png(filename = "Plot1.png", width = 600, height = 480);
+    png(filename = "Plot2.png", width = 600, height = 480);
     
     # plot the data without lower axis
     plot(totalsPerYear, type="o", xlim=xLimits, ylim=yLimits, xaxt="n");
@@ -38,10 +43,10 @@ createPlot1 <- function(){
     text(totalsPerYear, as.character(round(totalsPerYear[,2], 2)), cex=0.8, pos=3, offset = 0.3, col = "red");
     
     # decorate plot
-    title(main="Total emissions from PM2.5 in the United States");
+    title(main="Total emissions from PM2.5 in Baltimore City, Maryland");
     
     # closes png device
     dev.off();
     
-    message("Image Plot1.png generated!");
+    message("Image Plot2.png generated!");
 }
